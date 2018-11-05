77f47cf9e3901c6675317fe8d75e1a856decf465
Jiawen Zhu <zackzhu1@gmail.com>
2018-11-04 23:41:57 -0800

    find all the paths

---
+++ LC/src/Graph/Graph_1024_01.java
@@ -0,0 +1,58 @@
+package Graph;
+import java.util.*;
+
+public class Graph_1024_01 {
+	static class TreeNode {
+		TreeNode(int value) {
+			this.value = value;
+		}
+		int value;
+		TreeNode left, right;
+	}
+	
+	public static void findAllPaths(TreeNode root, List<TreeNode> path, List<List<TreeNode>> allPath) {
+		// base case
+		if (root == null) {
+			return;
+		}		
+		if (root.left == null && root.right == null) {
+			path.add(root);
+			allPath.add(new ArrayList<>(path));
+			path.remove(root); // Don't forget to backtrack !!!
+			return;
+		}
+		// recursion rule
+		path.add(root);
+		findAllPaths(root.left, path, allPath);
+		findAllPaths(root.right, path, allPath);
+		path.remove(root); // backtracking
+	}
+	
+	/*         1
+	 *      2       3
+	 *    4  5
+	 *   6
+	 */
+	
+	public static void main(String[] args) {
+		TreeNode root = new TreeNode(1);
+		TreeNode node2 = new TreeNode(2);
+		TreeNode node3 = new TreeNode(3);
+		TreeNode node4 = new TreeNode(4);
+		TreeNode node5 = new TreeNode(5);
+		TreeNode node6 = new TreeNode(6);
+		root.left = node2; root.right = node3;
+		node2.left = node4; node2.right = node5;
+		node4.left = node6;
+		List<TreeNode> curPath = new ArrayList<>();
+		List<List<TreeNode>> allPath = new ArrayList<>();
+		findAllPaths(root, curPath, allPath);
+		
+		for (List<TreeNode> path : allPath) {
+			for (TreeNode node : path) {
+				System.out.print(node.value + " ");
+			}
+			System.out.println();
+		}
+	}
+}
